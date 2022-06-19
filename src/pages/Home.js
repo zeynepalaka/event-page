e3dfd43c604e94aea5c1d499d0b83a5edfdb0c72
zeynepalaka <zeynepalakaa@gmail.com>
2022-06-20 00:12:58 +0300

    Fake data and css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,34 @@
+import categories from "../fake-data/categories.json"
+import events from "../fake-data/events.json"
+
 export function Home() {
-    return <h2>Home</h2>;
+    return (
+        <div className="container">
+            <div className="header">
+                <h2>Home</h2>
+                <div className="menu">
+                    <button>Etkinlikler</button>
+                    <button>Kategori</button>
+                    <button>Ara</button>
+                </div>
+            </div>
+
+            <div className="main">
+                <div className="categories">
+                    {
+                        categories.map((value) => <li key={value.id}>{value.title}</li>)
+                    }
+                </div>
+                <div className="events">
+                    {
+                        events.map((value) => <li key={value.id}>{value.title}</li>)
+                    }
+                </div>
+            </div>
+
+
+        </div>
+    );
 }
+
 
